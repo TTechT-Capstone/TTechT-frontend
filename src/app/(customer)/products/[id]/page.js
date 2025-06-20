93273e8286077214fad600620feb11e207e670b4
Ngoc Khanh <ngockhanh7643@gmail.com>
2025-06-20 09:28:41 +0700

    fix: error axios

--- src/app/(customer)/products/[id]/page.js
+++ src/app/(customer)/products/[id]/page.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import axios from 'axios';
+//import axios from 'axios';
 // import { useDispatch } from 'react-redux';
 // import { addItemToCart } from '../../redux/slices/cart.slice';
 // import useAuth from '../../hooks/useAuth';
