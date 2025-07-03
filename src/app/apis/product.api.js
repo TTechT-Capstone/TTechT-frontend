057ec19440a1e2d2b1eac7bff99c0fefe977334d
Ngoc Khanh <ngockhanh7643@gmail.com>
2025-07-03 22:24:02 +0700

    update: URL API

--- src/app/apis/product.api.js
+++ src/app/apis/product.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8082/api";
+const API_BASE_URL = "https://ttecht-backend.onrender.com/api";
 
 /**
  * Helper function to get the auth headers.
